90d577ca53861d8c0290d798aeb0557b5fcdbe32
cabral-filho <cabralfilhodj@gmail.com>
2021-11-19 21:17:52 -0300

    Update ExampleInstrumentedTest.kt

--- app/src/androidTest/java/com/nullpointer/exoplayerdemo/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/com/nullpointer/exoplayerdemo/ExampleInstrumentedTest.kt
@@ -1,4 +1,4 @@
-package com.nullpointer.exoplayerdemo
+package  com.nullpointer.exoplayerdemo
 
 import androidx.test.platform.app.InstrumentationRegistry
 import androidx.test.ext.junit.runners.AndroidJUnit4
